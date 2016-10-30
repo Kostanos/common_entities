abe217d61f521f3f38e3fc9d13e7e4e396293d48
Kostya Kostyushko <kostanos@gmail.com>
2016-10-29 21:39:52 -0400

    Added creating from object

--- entities/location.js
+++ entities/location.js
@@ -1,5 +1,15 @@
-exports.Location = function(longitude, latitude){
-  this.type = 'Point'; // Default MongoDB value. Should it be here??
+
+// @param {Object|number} longitude
+// @param {number} latitude
+// @param {string} type
+exports.Location = function(longitude, latitude, type){
+  // accept other object
+  if (typeof longitude == 'object'){
+    latitude = longitude.coordinates[1];
+    type = longitude.type;
+    longitude = longitude.coordinates[0];
+  };
+  this.type = type ? type : 'Point'; // Default MongoDB value. Should it be here??
   // [longitude, latitude]
   this.coordinates = [longitude, latitude];
   // Parse string value for location data
--- entities/street_address.js
+++ entities/street_address.js
@@ -1,11 +1,11 @@
-exports.StreetAddress = function(){
-  this.addressName = '';
-  this.streetAddress = '';
-  this.streetAddress2 = '';
-  this.house = '';
-  this.sector = '';
-  this.city = '';
-  this.state = '';
-  this.zipCode = '';
-  this.note = '';
+exports.StreetAddress = function(obj){
+  this.addressName = obj ? obj.addressName : '';
+  this.streetAddress = obj ? obj.streetAddress : '';
+  this.streetAddress2 = obj ? obj.streetAddress2 : '';
+  this.house = obj ? obj.house : '';
+  this.sector = obj ? obj.sector : '';
+  this.city = obj ? obj.city : '';
+  this.state = obj ? obj.state : '';
+  this.zipCode = obj ? obj.zipCode : '';
+  this.note = obj ? obj.note : '';
 };
